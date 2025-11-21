f6f11914937cf4959b544134b7be84e11d581246
Luke Ponga <developmentdesignsltd@gmail.com>
2025-11-21 04:47:18 +0000

    File changes

--- src/lib/seed-data.ts
+++ src/lib/seed-data.ts
@@ -1,3 +1,213 @@
 import type { JobApplication } from './types';
 
-export const seedApplications: Omit<JobApplication, 'id' | 'userId' | 'lastUpdated'>[] = [];
+export const seedApplications: Omit<JobApplication, 'id' | 'userId' | 'lastUpdated'>[] = [
+    {
+      company: 'Mountain V',
+      role: 'Casual Re',
+      location: 'Taupo, Wa',
+      dateApplied: '2025-11-17',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'Hamilton C',
+      role: 'Senior Ser',
+      location: 'Hamilton',
+      dateApplied: '2025-11-17',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'JD Sports',
+      role: 'Footwear',
+      location: 'Hamilton',
+      dateApplied: '2025-11-17',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'The Just G',
+      role: 'Christmas',
+      location: 'The Base,',
+      dateApplied: '2025-11-16',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'Bhogal Lim',
+      role: 'Courier Dr',
+      location: 'Te Rapa,',
+      dateApplied: '2025-11-16',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'Datacom',
+      role: 'Application',
+      location: 'Hamilton',
+      dateApplied: '2025-11-14',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'Red Path I',
+      role: 'Christmas',
+      location: 'Hamilton',
+      dateApplied: '2025-11-14',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'DEAR Mot',
+      role: 'General Fit',
+      location: 'Hamilton',
+      dateApplied: '2025-10-30',
+      status: 'Viewed',
+      url: ''
+    },
+    {
+      company: 'The Wareh',
+      role: 'Casual Te',
+      location: 'Te Rapa, H',
+      dateApplied: '2025-10-28',
+      status: 'Applied',
+      url: ''
+    },
+    {
+      company: 'Computer',
+      role: 'Customer',
+      location: 'Wellington',
+      dateApplied: '2025-10-25',
+      status: 'Not selected',
+      url: ''
+    },
+    {
+      company: 'Computer',
+      role: 'Service De',
+      location: 'Wellington',
+      dateApplied: '2025-10-25',
+      status: 'Not selected',
+      url: ''
+    },
+    {
+      company: 'Tech Dom',
+      role: 'IT Network',
+      location: 'Wellington',
+      dateApplied: '2025-10-03',
+      status: 'Not selected',
+      url: ''
+    },
+    {
+      company: 'Warehous',
+      role: 'Technolog',
+      location: 'Te Rapa, H',
+      dateApplied: '2025-10-03',
+      status: 'Interviewing',
+      url: ''
+    },
+    {
+      company: 'Computer',
+      role: 'IT Support',
+      location: 'Masterton',
+      dateApplied: '2025-10-20',
+      status: 'Not selected',
+      url: ''
+    },
+    {
+      company: 'Instant Wir',
+      role: 'Autoglass',
+      location: 'Hamilton',
+      dateApplied: '2025-09-19',
+      status: 'Interviewing',
+      url: ''
+    },
+    {
+      company: 'Hill Labs',
+      role: 'IT Support',
+      location: 'Hamilton L',
+      dateApplied: '2025-10-26',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'Cloud Torc',
+      role: 'Support Er',
+      location: 'Hamilton C',
+      dateApplied: '2025-11-01',
+      status: 'Unlikely to progress',
+      url: ''
+    },
+    {
+      company: 'Hampshire',
+      role: 'IT Operatic',
+      location: 'Wellington',
+      dateApplied: '2025-10-25',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'Aiscorp Lir',
+      role: 'Level 1 He',
+      location: 'Johnsonvil',
+      dateApplied: '2025-11-13',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'University',
+      role: 'Student Sy',
+      location: 'Hamilton E',
+      dateApplied: '2025-10-20',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'Montana G',
+      role: 'Packing &',
+      location: 'Te Rapa,',
+      dateApplied: '2025-10-25',
+      status: 'Unlikely to progress',
+      url: ''
+    },
+    {
+      company: 'The Instille',
+      role: 'Support Ar',
+      location: 'Hamilton C',
+      dateApplied: '2025-10-12',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'Repco Nev',
+      role: 'Part Time',
+      location: 'Hamilton,',
+      dateApplied: '2025-10-10',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'PODcom I',
+      role: 'Helpdesk',
+      location: 'Hamilton C',
+      dateApplied: '2025-10-05',
+      status: 'Unlikely to progress',
+      url: ''
+    },
+    {
+      company: 'Harvey No',
+      role: 'Technolog',
+      location: 'Hamilton C',
+      dateApplied: '2025-10-02',
+      status: 'Expired',
+      url: ''
+    },
+    {
+      company: 'Hamilton H',
+      role: 'Front of Ho',
+      location: 'Hamilton C',
+      dateApplied: '2025-10-02',
+      status: 'Expired',
+      url: ''
+    }
+  ];
+  
