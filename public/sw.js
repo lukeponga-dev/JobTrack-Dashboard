f4a0d12bdd440b4ce0a0a2bfdba5b2c477f7dfee
Luke Ponga <developmentdesignsltd@gmail.com>
2025-11-21 14:46:00 +0000

    Convert it to mobile app

--- public/sw.js
+++ public/sw.js
@@ -1,77 +1,69 @@
-// A basic service worker for PWA functionality
-
-const CACHE_NAME = 'jobtrack-cache-v1';
-const urlsToCache = [
+// Choose a cache name
+const cacheName = 'jobtrack-pwa-v1';
+// List the files to precache
+const precacheResources = [
   '/',
   '/dashboard',
   '/login',
   '/signup',
-  '/manifest.json',
-  '/favicon.ico',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/manifest.json'
 ];
 
+// When the service worker is installed, open a new cache and add all of the precache resources to it
 self.addEventListener('install', (event) => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(precacheResources);
+  })());
 });
 
+// When a fetch request is made, try to look for a response from the cache.
+// If a response is not found, fetch from the network.
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
+  // We only want to handle GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  event.respondWith((async () => {
+    const cache = await caches.open(cacheName);
+    
+    // Try to get the resource from the cache
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    // If the resource was not in the cache, try to get it from the network
+    try {
+        const fetchResponse = await fetch(event.request);
+        
+        // Save the resource in the cache and return it
+        // We can only cache responses with a 200 status code
+        if (fetchResponse.status === 200) {
+            await cache.put(event.request, fetchResponse.clone());
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        const fetchRequest = event.request.clone();
+        return fetchResponse;
 
-        return fetch(fetchRequest).then(
-          (response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-  );
+    } catch (e) {
+        // The network failed.
+        return new Response("Network error happened", {
+            "status" : 408,
+            "headers" : {"Content-Type" : "text/plain"}
+        });
+    }
+  })());
 });
 
+// Clean up old caches
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then((keyList) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
           }
         })
       );
